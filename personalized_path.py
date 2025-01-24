0001eea91fe2285f419648e0171e44b9c34413fe
DanielLambo <daniel.lambo@bulldogs.aamu.edu>
2025-01-24 17:24:58 -0600

    citations

--- personalized_path.py
+++ personalized_path.py
@@ -31,14 +31,31 @@ to better serve diverse user needs. Traditional library services often struggle 
 '''
 st.write(intro)
 
+st.divider()
+st.header("Assumptions: ")
+Assumptions = '''based on the context of this paper , our general assumptions for a learner will be
+1. Age Distribution and Learning Goals: Learners pursuing advanced topics like Deep Learning and Artificial Intelligence tend to be older, while those interested in foundational subjects such as Python Programming and Data Science are generally younger.
+
+2. Skill Level and Learning Outcomes: There appears to be a positive correlation between higher skill levels and better learning outcomes, suggesting that more advanced learners achieve greater success in their educational pursuits.
+
+3. Learning Path Complexity: Learners with higher skill levels often engage in more complex and longer learning paths, indicating a preference for in-depth study and comprehensive understanding of the subject matter.
+'''
+st.write("")
+
 
 
 
 citation_one = '''Jayashri Bagade, Poonam Chaudhari, Poonam Girish Fegade, Ranjit M.
 Gawande, Prachi P. Vast, Dipika R. Birari (2024). Adaptive Learning Technologies for Personalized
-Research Assistance in Libraries. Library Progress International, 44(1), 242-258.'''
+Research Assistance in Libraries. Library Progress International, 44(1), 242-258.
+
+"7 Things You Should Know About Adaptive Learning." 
+EDUCAUSE, 11 Jan. 2017, 
+https://library.educause.edu/resources/2017/1/7-things-you-should-know-about-adaptive-learning. 
+'''
 st.divider(
 )
+st.write(Assumptions)
 st.subheader("Works Cited:")
 st.caption(citation_one)
 
